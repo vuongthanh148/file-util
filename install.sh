450f0c0a32408f95df40e2247544f581197c8dc0
vuongthanh148 <vuongthanh148@gmail.com>
2024-10-24 01:22:02 +0700

    add install.sh file

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Name of the binary
+BINARY_NAME="futil"
+
+# Set execute permission
+chmod +x $BINARY_NAME
+
+# Move the binary to /usr/local/bin
+sudo mv $BINARY_NAME /usr/local/bin
+
+# Verify installation
+if command -v $BINARY_NAME &> /dev/null
+then
+    echo "$BINARY_NAME installed successfully"
+else
+    echo "Failed to install $BINARY_NAME"
+fi
